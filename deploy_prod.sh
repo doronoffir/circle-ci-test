3f572178df551f80951de0fc36214118f84473b9
Igor Chishkala <i@mateo.pp.ua>
2016-06-26 20:13:24 +0300

    First commit there

---
+++ deploy_prod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+ssh -v root@139.59.132.120 << EOF
+  
+  echo '1. Updating sources'
+  cd /repos/
+  git checkout --force master
+  git pull
+
+  echo "2. Restart apache"
+  sudo apache2ctl graceful
+
+  echo 'Done!'
+  
+EOF
