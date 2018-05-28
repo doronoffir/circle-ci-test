15e6892dcc59d4b95baef7e0137973779f9356ce
Doron Offir <doron.offir@doit-intl.com>
2018-05-28 12:06:57 +0300

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -4,12 +4,13 @@
 ssh -v doronoffir@35.205.152.169 << EOF
   
   echo '1. Updating sources'
-  cd /repos/circleci-demo/
-  git checkout --force master
-  git pull
+  mkdir alexg
+#  cd /repos/circleci-demo/
+ # git checkout --force master
+ # git pull
 
-  echo "2. Restart apache"
-  sudo apache2ctl graceful
+  #echo "2. Restart apache"
+ # sudo apache2ctl graceful
 
   echo 'Done!'
   echo 'Done!'
